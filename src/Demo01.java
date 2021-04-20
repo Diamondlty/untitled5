8b27de723901d3d7272ee1faddf8a3aed94d95cf
Diamondlty <2816981693@qq.com>
2021-04-20 17:49:36 +0800

    Initial commit

---
+++ src/Demo01.java
@@ -0,0 +1,9 @@
+public class Demo01 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int y = 0; y < i; y++) {
+                System.out.println(i +"*"+y+"="+i*y);
+            }
+        }
+    }
+}
